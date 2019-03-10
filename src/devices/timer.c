efea3137a2b62d9aa452bb4a219c86c63bc9e38b
sihyun-yu <yusihyunc@kaist.ac.kr>
2019-03-10 22:06:56 +0900

    fixing recent cpu

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -141,6 +141,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
   ticks++;
   if (thread_mlfqs) {
    if (ticks % TIMER_FREQ == 0 ) {
+      thread_current()->recent_cpu += (1<<14);
       thread_calculate_load_avg();
       thread_calculate_recent_cpu ();
     }
