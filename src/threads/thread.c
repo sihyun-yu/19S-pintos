8e58345fc7f8a1311738eee85111f645c0f36a79
sihyun-yu <yusihyunc@kaist.ac.kr>
2019-03-11 02:12:09 +0900

    fixing mlfqs

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -734,14 +734,14 @@ void test_max_priority(void) {
     ASSERT(list_entry (list_front (&ready_list), struct thread, elem) != NULL)
     struct thread *t = list_entry(list_front (&ready_list), struct thread, elem);
 
-    if (intr_context()) {
+    /*if (intr_context()) {
       thread_ticks++;
       if ( thread_current()->priority < t->priority || (thread_ticks >= TIME_SLICE && thread_current()->priority == t->priority) )
       {
          intr_yield_on_return();
       }
       return;
-    }
+    }*/
 
     if (thread_current ()->priority < list_entry (list_front (&ready_list), struct thread, elem)->priority){
     thread_yield ();
