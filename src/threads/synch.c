cd1e81b9fb52980946a617d5d92ba2395352178b
sihyun-yu <yusihyunc@kaist.ac.kr>
2019-03-11 00:14:40 +0900

    Fixing mlfqs

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -214,7 +214,7 @@ lock_acquire (struct lock *lock)
   if (lock->holder == NULL) lock->priority = cur->priority;
     
 
-  if(thread_mlfqs && lock->holder) {
+  if(!thread_mlfqs && lock->holder) {
     priority_donation(lock);
     list_insert_ordered(&lock->holder->lock_list, &lock->lock_elem, lock_priority_compare, 0);
   }
@@ -260,12 +260,10 @@ lock_release (struct lock *lock)
   enum intr_level old_level = intr_disable();
 
   lock->holder = NULL;
-  if (thread_mlfqs) {
+  sema_up (&lock->semaphore);
+  if (!thread_mlfqs) {
   priority_donation_finished(lock);
   }
-
-  sema_up (&lock->semaphore);
-
   intr_set_level(old_level);
 
 }
