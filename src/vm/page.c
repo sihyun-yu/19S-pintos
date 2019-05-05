3e68a7e7716ffe5b0a7c86357cd207a65ceef342
김지호 <kimjiho@gimjihoui-MacBook-Pro.local>
2019-05-06 00:05:14 +0900

    repush

--- src/vm/page.c
+++ src/vm/page.c
@@ -160,3 +160,27 @@ install_page (void *upage, void *kpage, bool writable)
   return (pagedir_get_page (t->pagedir, upage) == NULL
           && pagedir_set_page (t->pagedir, upage, kpage, writable));
 }
+
+
+bool stack_growth(struct hash *supt, void *addr){
+	struct sup_page_table_entry *spte = allocate_page(supt, addr);
+	
+	if (spte == NULL)
+		return false;
+
+	uint8_t *kpage = allocate_frame(PAL_USER | PAL_ZERO, spte);
+	if (kpage == NULL){
+		free_page(spte);
+		return false;
+	}
+	//if (/*hash_find(supt, spte->hs_elem) != NULL || */!hash_insert(supt, spte->hs_elem)){
+	//if supt has the spte, return NULL. if supt doesn't have the spte, returns modified supt.
+	//	free_page(spte);
+	//	free_frame(kpage);
+	//	return false;
+	//}
+	return true;
+}
+
+
+
